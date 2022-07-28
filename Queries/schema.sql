c0a63865a8dc36bb30181b9a3c6d287f904ee26c
Francesca Walz <francescawalz@Francescas-iMac.local>
2022-07-28 08:49:34 -0400

    adding new data files

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -42,10 +42,10 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 );
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR(18) NOT NULL,
+	title VARCHAR(50) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	PRIMARY KEY (emp_no),
-	UNIQUE (title)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
 );
 SELECT * FROM departments;
