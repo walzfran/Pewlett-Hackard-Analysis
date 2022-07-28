18817a7b54dc0942973b1007b03138a22f39f882
Francesca Walz <francescawalz@Francescas-iMac.local>
2022-07-28 18:34:49 -0400

    adding new files

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,54 @@
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, e.birth_date DESC;
+
+--Deliverable 1, 2nd Table
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+ti.title
+INTO unique_titles
+FROM employees as e
+LEFT JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND ti.to_date = ('9999-01-01')
+ORDER BY e.emp_no, e.birth_date DESC;
+
+-- Deliverable 1, 3rd Table
+SELECT 
+COUNT(ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT DESC;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility 
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND ti.to_date = ('9999-01-01')
+ORDER BY e.emp_no, e.birth_date DESC;
+
+select * from mentorship_eligibility;
