9d8834e7fa5d015ddbe83e2c4174b36b47135799
walzfran <106924615+walzfran@users.noreply.github.com>
2022-07-28 21:57:13 -0400

    Update support_query_1.sql

--- Queries/support_query_1.sql
+++ Queries/support_query_1.sql
@@ -1,8 +1,8 @@
-SELECT COUNT(ce.emp_no),
+SELECT COUNT(e.emp_no),
 ti.title
-FROM current_emp as ce
+FROM employees as e
 INNER JOIN titles as ti
-ON ce.emp_no = ti.emp_no
+ON e.emp_no = ti.emp_no
 WHERE ti.to_date = ('9999-01-01')
 GROUP BY ti.title
 ORDER BY ti.title;
